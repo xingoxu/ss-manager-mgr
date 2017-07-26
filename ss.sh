3abe820abd85e50af567280dfc4379be28ded849
xingoxu <xingoxu@gmail.com>
2017-07-26 22:45:11 +0800

    more targets

--- ss.sh
+++ ss.sh
@@ -1,4 +1,4 @@
-nohup ss-manager -m chacha20-ietf -u --manager-address 127.0.0.1:1234 &
+nohup ss-manager -m chacha20-ietf-poly1305 -u --manager-address 127.0.0.1:1234 &
 mkdir ~/.ssmgr/
 cp ss.yml ~/.ssmgr/
 ssmgr -c ss.yml
